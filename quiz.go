f8216296411ff3496c19e983c8ba3ac28b61a1ec
Daniel Schwartz <me@dansdomain.net>
2022-02-12 17:00:10 +0200

    Added the timer funcitonality

--- quiz.go
+++ quiz.go
@@ -7,31 +7,42 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	filepath := flag.String("fpath", "problems.csv", "the file path to the csv containing problems")
-	timer := flag.Int("timer", -1, "Set the timer in seconds, default -1 means disabled")
+	timeLimit := flag.Int("timer", 30, "the timer in seconds")
 	flag.Parse()
 	fmt.Printf("the filepath is %v\n", *filepath)
-	fmt.Printf("the timer is %v\n", *timer)
+	fmt.Printf("the timer is %v\n", *timeLimit)
 
-	records, err := readData(*filepath)
+	lines, err := readData(*filepath)
 	if err != nil {
 		exit(fmt.Sprintf("\nFailed to read file %s\n", *filepath), err)
 	}
-	questions := parseLinesIntoProblems(records)
-
+	questions := parseLinesIntoProblems(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	countCorrect := 0
-
-	for i, p := range questions {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			countCorrect++
+	problemloop:
+		for i, p := range questions {
+			fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+			ansChan := make(chan string)
+			go func() {
+				var answer string
+				fmt.Scanf("%s\n", &answer)
+				ansChan <- answer
+			}()
+			select {
+			case <- timer.C:
+				fmt.Println()
+				break problemloop
+			case answer := <- ansChan:
+				if answer == p.a{
+					countCorrect++
+				}
+			}
 		}
-	}
 
 	fmt.Printf("You answered %d correct out of %d\n", countCorrect, len(questions))
 }
